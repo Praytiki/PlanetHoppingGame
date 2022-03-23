b94075077c1cf6ccfff982f7d11f1d57504b4915
aryanch22 <mikesteve726@gmail.com>
2022-03-22 21:21:11 -0400

    init

---
+++ Planet.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+/**
+ * Planet class
+ */
+public class Planet {
+    public static void main(String[] args) {
+        System.out.println("You're on Planet Aries!");
+    }
+}
